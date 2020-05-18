5ffe04061f8f24f69ad80ceeae21cff6ec92b85f
AbilashBodapati <bodapatiabilash98@gmail.com>
2020-05-18 19:44:22 -0400

    Get all data from 3 webpages of microcenter for just CPU component.

--- webScrapper.py
+++ webScrapper.py
@@ -1,79 +1,109 @@
 '''
     File name: webScrapper.py
     Author name: Abilash Bodapati
-    Description: <TO BE WRITTEN AT THE END>
+    Description: This is a script that scraps data from websites to get tech deals.
 '''
 
 # Import all the necessary libraries
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import json
 
 # URL I want to initially scrap. Intel CPU page
-microcenter_url = 'https://www.microcenter.com/search/search_results.aspx?N=4294966995+4294820689&sortby=rating'
-
-# query the website and return the html to the variable 'page'
-uClient = uReq(microcenter_url)
-
-# Raw html page of the Intel CPU results page
-page_html = uClient.read()
-
-# Close the uClient Connection to the website.
-uClient.close()
-
-# HTML parsing
-page_soup = soup(page_html, "html.parser")
-
-# Create a Dictionary of the product list
-totalProductList = []
-
-###-----FORMATING DATA------
-## Product name
-## Product Price
-## Product Saved Price
-## Product Image
-## Product Sortby
-'''
-    JSON format:
-    {
-        'product name': <String>,
-        'product price': <Integer>,
-        'product saved price': <Integer>,
-        'product image url': <String>,
-        'product sort group': <String>,
-    }
-
-'''
-
-# Grabs product name 
-productNames_info = page_soup.findAll("div", {"class": "details"})
-productName = productNames_info[0].div.h2.a.text
-
-# Grabs product price
-productPrice_info = page_soup.findAll("span", {"itemprop": "price"})
-productPrice = productPrice_info[0].text.split('$')[1]
-
-# Grabs product saved price
-productSavedPrice_info = page_soup.findAll("span", {"class": "savings"})
-productSavedPrice = productSavedPrice_info[0].text.split('$')[1]
-
-# Grabs product image url
-productImage_info = page_soup.findAll("a", {"class": "image"})
-productImage = productImage_info[0].find("img")['src']
-
-# Grabs product group
-productGroup_info = page_soup.findAll("div", {"class": "pre-dropdown Sort boosted"})
-productGroup = productGroup_info[0].div.button.span.text
+microcenter_url = ['https://www.microcenter.com/search/search_results.aspx?N=4294966995+4294820689&sortby=rating',
+                   'https://www.microcenter.com/search/search_results.aspx?N=4294966995+4294820689&sortby=match',
+                   'https://www.microcenter.com/search/search_results.aspx?N=4294966995+4294820689&sortby=pricehigh']
 
 
+# Create a dictionary of the product dictionaries
+totalProductList = {}
 
-totalProductList = {
-    "Product 1": {
-        'Product name': productName,
-        'Product price': productPrice,
-        'Product saved price': productSavedPrice,
-        'Product image url': productImage,
-        'Product sort group': productGroup
-    }
-}
+for url in microcenter_url:
+    # query the website and return the html to the variable 'page'
+    uClient = uReq(url)
 
-print(totalProductList)
+    # Raw html page of the Intel CPU results page
+    page_html = uClient.read()
+
+    # Close the uClient Connection to the website.
+    uClient.close()
+
+    # HTML parsing
+    page_soup = soup(page_html, "html.parser")
+
+    ###-----FORMATING DATA------
+    ## Product name
+    ## Product Price
+    ## Product Saved Price
+    ## Product Image
+    ## Product Group by
+    ## Product Retrevied from
+    '''
+        JSON format:
+        {
+            'product name': <String>,
+            'product price': <Integer>,
+            'product saved price': <Integer>,
+            'product image url': <String>,
+            'product sort group': <String>,
+        }
+
+    '''
+
+    # Grabs product name 
+    productNames_info = page_soup.findAll("div", {"class": "details"})
+    # Grabs product price
+    productPrice_info = page_soup.findAll("span", {"itemprop": "price"})
+    # Grabs product saved price
+    productSavedPrice_info = page_soup.findAll("span", {"class": "savings"})
+    # Grabs product image url
+    productImage_info = page_soup.findAll("a", {"class": "image"})
+
+    # Grabs product group
+    productGroup_info = page_soup.findAll("div", {"class": "pre-dropdown Sort boosted"})
+    # Product Group By
+    productGroup = productGroup_info[0].div.button.span.text
+
+    # A for-loop to iterate through the list created above
+    for i in range(10):
+        # Product Name
+        productName = productNames_info[i].div.h2.a.text
+        # Product Price
+        productPrice = productPrice_info[i].text.split('$')[1]
+        # product Saved Price
+        productSavedPrice = productSavedPrice_info[i].text.split('$')[1]
+        # Product Image URL
+        productImage = productImage_info[i].find("img")['src']
+        
+        productIter = '{} Product {}'.format(productGroup, i+1)
+        # Add the product in to the list
+        productDict = {
+            productIter: {
+                'Product name': productName,
+                'Product price': productPrice,
+                'Product saved price': productSavedPrice,
+                'Product image url': productImage,
+                'Product sort group': productGroup
+            }
+        }
+
+        # Append the created dictionary to the total list
+        totalProductList.update(productDict)
+
+    print(totalProductList)
+    print("")
+
+
+
+
+
+
+
+
+# Creating a JSON file
+jsonFormatedProductList = json.dumps(totalProductList, indent=4)
+
+# Writing to sample.json 
+with open("products.json", "w") as outfile: 
+    outfile.write(jsonFormatedProductList)
+ 
